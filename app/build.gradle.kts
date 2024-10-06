5e9030a1bb4565c704e209467de509cbba9a07c1
sergioflorez30 <sergiofflorez2005@gmail.com>
2024-10-06 15:45:18 -0500

    Estructura de la aplicacion

---
+++ app/build.gradle.kts
@@ -0,0 +1,82 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.example.classmate"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.classmate"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    // Todo el módulo de navegación incluye los composables NavHost y NavController. También permite instanciar de forma simple ViewModel()
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+// Incluye los objetos LiveData y MutableLiveData. Además del método observeAsState()
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.8")
+// Nos permite cargar imágenes de la web en un composable de imagen de forma asincrónica
+    implementation("io.coil-kt:coil-compose:2.7.0")
+// Nos permite serializar objetos en JSON
+    implementation("com.google.code.gson:gson:2.11.0")
+//Nos permite usar las corutinas en el contexto de ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
+//Nos permite usar await para llamados a la red
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.9.0")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
