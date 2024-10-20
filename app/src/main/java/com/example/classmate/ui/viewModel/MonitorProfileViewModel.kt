038b5279a0a7913edfeb85b0c3d06485697195dc
vortzzz <v.ortizzuleta@gmail.com>
2024-10-20 18:34:20 -0500

    initial commit

---
+++ app/src/main/java/com/example/classmate/ui/viewModel/MonitorProfileViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.classmate.ui.viewModel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.classmate.data.repository.MonitorRepository
+import com.example.classmate.data.repository.MonitorRepositoryImpl
+import com.example.classmate.domain.model.Monitor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MonitorProfileViewModel(val repo: MonitorRepository = MonitorRepositoryImpl()):ViewModel() {
+    private val _monitor = MutableLiveData<Monitor?>(Monitor())
+    val student: LiveData<Monitor?> get() = _monitor
+    fun showMonitorInformation() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val currentUser = repo.getCurrentMonitor()
+                withContext(Dispatchers.Main) {
+                    if (currentUser != null) {
+                        _monitor.value = currentUser
+                    } else {
+                        _monitor.value = null
+                    }
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    _monitor.value = null
+                    Log.e("ViewModel", "Error fetching monitor info: ${e.message}")
+                }
+
+            }
+        }
+    }
+}
