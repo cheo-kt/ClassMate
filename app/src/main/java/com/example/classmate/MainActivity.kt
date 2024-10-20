41fad9e6ea4e4ea9812b827f3ce451463b782e84
sergioflorez30 <sergiofflorez2005@gmail.com>
2024-10-20 09:47:32 -0500

    cambio de inicio, ya no aguanto maaaaaaaaaaaaaas

--- app/src/main/java/com/example/classmate/MainActivity.kt
+++ app/src/main/java/com/example/classmate/MainActivity.kt
@@ -73,7 +73,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "introductionMonitor") {
+    NavHost(navController = navController, startDestination = "signing") {
         composable("signup") { StudentSignupScreen(navController) } //Registro estudiante
         composable("signing") { StudentMonitorSigninScreen(navController) } //Login
         composable("introductionStudent") { IntroductionsStudentScreen(navController) } //introducción Estudiante
