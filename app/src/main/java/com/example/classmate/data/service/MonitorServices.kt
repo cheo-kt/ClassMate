b75bad3a893a47b31c625707d818ad3437b59465
juaca2004 <114953837+juaca2004@users.noreply.github.com>
2024-10-21 10:44:08 -0500

    add dependency

--- app/src/main/java/com/example/classmate/data/service/MonitorServices.kt
+++ app/src/main/java/com/example/classmate/data/service/MonitorServices.kt
@@ -5,6 +5,7 @@ import com.example.classmate.domain.model.Monitor
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+
 import kotlinx.coroutines.tasks.await
 
 interface MonitorServices {
