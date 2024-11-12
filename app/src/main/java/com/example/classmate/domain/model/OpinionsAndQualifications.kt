7d903fbfd1b56e880d72e9534fdb166a2880054b
sergioflorez30 <sergiofflorez2005@gmail.com>
2024-11-12 18:44:11 -0500

    preview prefile monitor

--- app/src/main/java/com/example/classmate/domain/model/OpinionsAndQualifications.kt
+++ app/src/main/java/com/example/classmate/domain/model/OpinionsAndQualifications.kt
@@ -4,4 +4,7 @@ class OpinionsAndQualifications(
     var id:String="",
     var calification: Int = 0,
     var opinion: String = ""
-)
+){
+    constructor() : this("", 0,"")
+
+}
---
+++ app/src/main/java/com/example/classmate/ui/viewModel/PreviewMonitorViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.classmate.ui.viewModel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.classmate.data.repository.MonitorRepository
+import com.example.classmate.data.repository.MonitorRepositoryImpl
+import com.example.classmate.domain.model.Monitor
+import com.example.classmate.domain.model.Notification
+import com.example.classmate.domain.model.OpinionsAndQualifications
+import com.google.firebase.auth.FirebaseAuthException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class PreviewMonitorViewModel(val repo: MonitorRepository = MonitorRepositoryImpl()): ViewModel() {
+    private val _monitor = MutableLiveData<Monitor?>(Monitor())
+    private var lastopinion:OpinionsAndQualifications? = null
+    val monitor: LiveData<Monitor?> get() = _monitor
+    val monitorState = MutableLiveData(0)
+    val limit = 2;
+    private val _opinionList = MutableLiveData(listOf<OpinionsAndQualifications?>())
+    val opinionList: LiveData<List<OpinionsAndQualifications?>> get() = _opinionList
+
+
+
+    fun getOpinionMonitor(monitorId: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            viewModelScope.launch(Dispatchers.Main) { monitorState.value = 1 }
+            try {
+                val newOpinion = repo.loadMoreOpinions(limit, lastopinion,monitorId)
+                withContext(Dispatchers.Main) {
+                    if (newOpinion.isNotEmpty()) {
+                        _opinionList.value = _opinionList.value.orEmpty() + newOpinion
+                        lastopinion= _opinionList.value?.lastOrNull()
+                        viewModelScope.launch(Dispatchers.Main) { monitorState.value = 3 }
+                    }
+                    else{
+                        viewModelScope.launch(Dispatchers.Main) { monitorState.value = 0 }
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                viewModelScope.launch(Dispatchers.Main) { monitorState.value = 2 }
+            }
+            Log.e(">>>>", "Estoy en viewmodel")
+        }
+    }
+
+}
+
+
+
+
+
+
